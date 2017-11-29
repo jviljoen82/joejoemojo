b8bf0d9e577c92436d20c063dd36558d96daff5a
Johan Viljoen <cj_viljoen@mweb.co.za>
2017-11-29 21:38:18 +0200

    Initial commit

---
+++ app/src/main/java/com/trading/johanv/joejoemojo/DisplayMessageActivity.java
@@ -0,0 +1,23 @@
+package com.trading.johanv.joejoemojo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get intent from parent class
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // set textview
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
---
+++ app/src/main/java/com/trading/johanv/joejoemojo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.trading.johanv.joejoemojo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.trading.johanv.joejoemojo.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Send Button click event */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
