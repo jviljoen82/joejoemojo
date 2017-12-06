f15d29cd009300fb8a52121c66ce1a316e7bab7b
Johan Viljoen <Johan.Viljoen@imqs.co.za>
2017-12-06 11:27:07 +0200

    More websocket stuff

--- app/src/main/java/com/trading/johanv/joejoemojo/MainActivity.java
+++ app/src/main/java/com/trading/johanv/joejoemojo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.trading.johanv.joejoemojo;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 import java.net.URI;
@@ -10,7 +13,7 @@ import java.net.URISyntaxException;
 
 
 public class MainActivity extends AppCompatActivity {
-    public static WebSocketClient webSocketClient;
+    private WebSocketClient mWebSocketClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,42 +25,42 @@ public class MainActivity extends AppCompatActivity {
 
     private void connectWebSocket() {
         URI uri;
-
         try {
             uri = new URI("wss://ws.binary.com/websockets/v3?app_id=11543");
-            Log.println(Log.INFO, "URI", uri.toString());
-        } catch (URISyntaxException uriEx) {
-            uriEx.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
             return;
         }
 
-        webSocketClient = new WebSocketClient(uri) {
+        mWebSocketClient = new WebSocketClient(uri) {
             @Override
-            public void onOpen(ServerHandshake handShakeData) {
-                webSocketClient.send("ping: 1");
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
             }
 
             @Override
-            public void onMessage(String message) {
-
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView textView = (TextView)findViewById(R.id.textView);
+                        textView.setText(textView.getText() + "\n" + message);
+                    }
+                });
             }
 
             @Override
-            public void onClose(int code, String reason, boolean remote) {
-
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
             }
 
             @Override
-            public void onError(Exception ex) {
-
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
             }
         };
-
-        try {
-            webSocketClient.connect();
-            Log.println(Log.INFO,"WebSocket", "CONNECTION SUCCESSFUL");
-        } catch (Exception webEx) {
-            Log.println(Log.ERROR, "WebSocket", "not connecting" + webEx.getMessage());
-        }
+        mWebSocketClient.connect();
     }
 }
